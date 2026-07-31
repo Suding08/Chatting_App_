828203d0f618346243fe197b020e912b69e6724c
Suding08 <gsudin52@gmail.com>
2026-08-01 01:14:14 +0530

    Delete server/controllers/lib directory

--- server/controllers/lib/utils.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-
-//Function to generate JWT token
-
-export const generateToken = (userId) => {
-const token = jwt.sign({userId}, process.env.JWT_SECRET);
-return token;
-}
