375119977e6ae36673ecc920c9a43ea46fb7f8e0
Suding08 <gsudin52@gmail.com>
2026-08-01 01:45:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import { AuthContext } from '../context/AuthContext'
 function App(){
   const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
@@ -22,4 +22,6 @@ function App(){
 }
 
 export default App
+
+
 
